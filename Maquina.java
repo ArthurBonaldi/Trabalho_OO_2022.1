aa0c8543f93d04d5130c06ec494cfb43f0676d93
Miguel Sales <miguelsales02@gmail.com>
2022-06-08 17:03:07 -0300

    Adiciona comentarios com informacoes do aluno e alterações no arquivo POM

--- Maquina.java
+++ Maquina.java
@@ -1,6 +1,10 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ Alunos:
+Miguel Sales de Almeida Lopes - 202076024
+Arthur Bonaldi Fernandes - 202065567C
+Joel Henrique Nunes de Oliveira Silva - 202076030
+
+Link do repositório: https://github.com/ArthurBonaldi/Trabalho_OO_2022.1
  */
 package ufjf.dcc025.trabalhooo;
 
