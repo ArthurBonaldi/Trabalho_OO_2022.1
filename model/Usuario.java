75b41007b0677116e7dfaa19516877c47a633349
ArthurBonaldi <arthur.bonaldi@ice.ufjf.br>
2022-07-19 20:31:49 -0300

    ajustando formatacao mvc

---
+++ model/Usuario.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ufjf.dcc025.trabalhooo.model;
+
+
+
+/**
+ *
+ * @author salesmiguelz
+ */
+public class Usuario  {
+    private static int numUsers; 
+    private int idUser;
+    private String nome;
+    private String email;
+    private String senha;
+
+    public Usuario(){
+       numUsers++;
+       this.nome = "";
+       this.senha ="";
+       this.email ="";
+       this.idUser =0; 
+      
+    }
+    public int getId() {
+        return idUser;
+    } 
+ 
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) throws Exception{
+       if(senha.length() >= 6 && senha.length() <= 12 ){
+           this.senha = senha;
+       }
+       else{
+           throw new Exception("Senha Invalida");
+       }
+    }
+   
+    
+    public String getNome(){
+        return this.nome;
+    }
+    
+    public void setNome(String nome){
+        if(nome.length() > 3){
+            this.nome = nome;
+        } else{
+            System.out.println("O nome do cliente precisa ter mais que 3 caracteres.");
+        }
+    }
+    
+    public String getEmail(){
+        return this.email;
+    }
+    
+    public void setEmail(String email){
+        if(email.contains("@") && email.contains(".com")){
+            this.email = email;
+        } else{
+            System.out.println("Email inválido!");
+        }
+    }
+    
+    public void setIdUser(String idUser) {
+        this.idUser = hashCode();
+    }
+   
+}
