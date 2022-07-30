6959633d8b5489a8973eb07a6697883bae159cd8
Joel-Henrique <joel.henrique@estudante.ufjf.br>
2022-07-30 13:48:41 -0300

    adiciona e trata exceções do produto

--- controller/ProdutoController.java
+++ controller/ProdutoController.java
@@ -1,24 +1,33 @@
 package ufjf.dcc025.trabalhooo.controller;
 
-import ufjf.dcc025.trabalhooo.model.EmailInvalido;
-import ufjf.dcc025.trabalhooo.model.NomeInvalido;
-import ufjf.dcc025.trabalhooo.model.Produto;
-import ufjf.dcc025.trabalhooo.model.Usuario;
+import ufjf.dcc025.trabalhooo.model.*;
 
 public class ProdutoController {
-    public static void create(String nome, int quantidade, double preco){
+    public static void create(String nome, String quantidade, String preco){
         Produto produto = new Produto();
-        produto.setNome(nome);
-        produto.setQuantidade(quantidade);
-        produto.setPreco(preco);
-        Produto.addProduto(produto);
+        try {
+            produto.setNome(nome);
+            produto.setQuantidade(quantidade);
+            produto.setPreco(preco);
+            Produto.addProduto(produto);
+        }catch (NomeInvalido Nm){
+            System.out.println(Nm.getMessage());
+        }catch (NumeroInvalido Num) {
+            System.out.println(Num.getMessage());
+        }
     }
 
-    public static void update(String nome, int quantidade, double preco, int id){
+    public static void update(String nome, String quantidade, String preco, int id){
         Produto produto = Produto.getProduto(id);
-        produto.setNome(nome);
-        produto.setQuantidade(quantidade);
-        produto.setPreco(preco);
+        try {
+            produto.setNome(nome);
+            produto.setQuantidade(quantidade);
+            produto.setPreco(preco);
+        }catch (NomeInvalido Nm){
+            System.out.println(Nm.getMessage());
+        }catch (NumeroInvalido Num) {
+            System.out.println(Num.getMessage());
+        }
     }
 
     public static void delete(int id){
