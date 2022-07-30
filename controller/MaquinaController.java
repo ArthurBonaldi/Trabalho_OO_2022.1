a80501d690432941276171c6315cc98f3e109df0
Joel-Henrique <joel.henrique@estudante.ufjf.br>
2022-07-30 13:55:38 -0300

    adiciona e trata exceções da maquina

--- controller/MaquinaController.java
+++ controller/MaquinaController.java
@@ -3,21 +3,35 @@ package ufjf.dcc025.trabalhooo.controller;
 import java.util.Date;
 
 import ufjf.dcc025.trabalhooo.model.Maquina;
+import ufjf.dcc025.trabalhooo.model.NomeInvalido;
+import ufjf.dcc025.trabalhooo.model.NumeroInvalido;
 
 public class MaquinaController {
-    public static void create(String nome, double peso, Date dataDeManutencao){
+    public static void create(String nome, String peso, Date dataDeManutencao){
         Maquina maquina = new Maquina();
-        maquina.setNome(nome);
-        maquina.setPeso(peso);
-        maquina.setDataDeManutencao(dataDeManutencao);
-        Maquina.addMaquina(maquina);
+        try {
+            maquina.setNome(nome);
+            maquina.setPeso(peso);
+            maquina.setDataDeManutencao(dataDeManutencao);
+            Maquina.addMaquina(maquina);
+        }catch (NomeInvalido Nm){
+            System.out.println(Nm.getMessage());
+        }catch (NumeroInvalido Num) {
+            System.out.println(Num.getMessage());
+        }
     }
 
-    public static void update(String nome, double peso, Date dataDeManutencao, int id){
+    public static void update(String nome, String peso, Date dataDeManutencao, int id){
         Maquina maquina = Maquina.getMaquina(id);
-        maquina.setNome(nome);
-        maquina.setPeso(peso);
-        maquina.setDataDeManutencao(dataDeManutencao);
+        try {
+            maquina.setNome(nome);
+            maquina.setPeso(peso);
+            maquina.setDataDeManutencao(dataDeManutencao);
+        }catch (NomeInvalido Nm){
+            System.out.println(Nm.getMessage());
+        }catch (NumeroInvalido Num) {
+            System.out.println(Num.getMessage());
+        }
     }
 
     public static void delete(int id){
