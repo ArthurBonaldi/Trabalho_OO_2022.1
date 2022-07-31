4b2e156af0cf4da12d63c1927bce799c9960b3c8
Joel-Henrique <joel.henrique@estudante.ufjf.br>
2022-07-31 20:10:10 -0300

    Update controller PF

--- controller/PFController.java
+++ controller/PFController.java
@@ -4,10 +4,53 @@
  */
 package ufjf.dcc025.trabalhooo.controller;
 import  ufjf.dcc025.trabalhooo.model.PF;
+import ufjf.dcc025.trabalhooo.model.*;
 /**
  *
  * @author arthu
  */
 public class PFController {
+    public static PF create(String nome, String email,String CPF,String data ){
+        PF pessoaF = new PF();
+        try {
+            pessoaF.setNome(nome);
+            pessoaF.setEmail(email);
+            pessoaF.setCPF(CPF);
+            pessoaF.setDate(data);
+
+            PF.addUser(pessoaF);
+
+        }catch (NomeInvalido Nm){
+            System.out.println(Nm.getMessage());
+        }catch (EmailInvalido Em){
+            System.out.println(Em.getMessage());
+        }catch (CPFInvalido cp){
+            System.out.println(cp.getMessage());
+        }catch (DataInvalida dt){
+            System.out.println(dt.getMessage());
+        }
+        return pessoaF;
+    }
+    public static PF update(String nome, String email,String CPF,String data ) {
+        PF pessoaF = Cliente.getUser(id);
+        try {
+            pessoaF.setNome(nome);
+            pessoaF.setEmail(email);
+            pessoaF.setCPF(CPF);
+            pessoaF.setDate(data);
+
+            //Cliente.addUser(pessoaF);
+
+        } catch (NomeInvalido Nm) {
+            System.out.println(Nm.getMessage());
+        } catch (EmailInvalido Em) {
+            System.out.println(Em.getMessage());
+        } catch (CPFInvalido cp) {
+            System.out.println(cp.getMessage());
+        } catch (DataInvalida dt) {
+            System.out.println(dt.getMessage());
+        }
+        return pessoaF;
+    }
     
 }
